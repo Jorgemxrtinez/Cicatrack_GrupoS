32909f57ce50a782f3a7cb163e7031a0f3061d12
Jorge <jorgevje2@gmail.com>
2025-12-01 10:47:02 +0100

    Primer commit: Cicatrack

---
+++ src/main/java/com/GrupoS/Jorge_Cicatrack/JorgeCicatrackApplication.java
@@ -0,0 +1,13 @@
+package com.GrupoS.Jorge_Cicatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgeCicatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JorgeCicatrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GrupoS/Jorge_Cicatrack/JorgeCicatrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrupoS.Jorge_Cicatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorgeCicatrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
